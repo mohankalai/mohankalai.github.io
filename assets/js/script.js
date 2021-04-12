a3ceaf5de1205912fab4bc7222dcc1f1c6c57a6c
shivaak <shivainfotech12@gmail.com>
2021-04-12 08:48:52 +0530

    updated countUp feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -384,13 +384,23 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2020/10/26 06:00', function(event) {
-            var $this = $(this).html(event.strftime(''
+		
+		$('#clock').countdown('2020/10/26 06:00', {elapse:true}).on('update.countdown', function(event) {
+					var $this = $(this);
+					if (event.elapsed) {
+						var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
             + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
             + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
-        });
+					} else {
+						var $this = $(this).html(event.strftime(''
+            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
+            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
+            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+					}
+		});
     }
 
 
